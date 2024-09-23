336c310f5fcb0e431846e5bfc16ccb92da90c11b
EMP-3RR0R <113849183+EMP-3RR0R@users.noreply.github.com>
2024-09-23 19:51:10 +0500

    Добавление /help
    Добавил в MessageHandlers.java map с парами "команда-описание", а также реализовал составление текста для /help через изменяемую строку StringBuilder и её вывод. Также реализовал описание команд в отдельном блоке кода (static {...}).

--- src/main/java/bot/MessageHandlers.java
+++ src/main/java/bot/MessageHandlers.java
@@ -3,8 +3,20 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.request.SendMessage;
 
-public class MessageHandlers {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MessageHandlers {
+    
+    private static final Map<String, String> commandsMap = new HashMap<>();
+
+    static {
+        commandsMap.put("/start", "Команда для начала работы.");
+        commandsMap.put("/authors", "Список авторов бота.");
+        commandsMap.put("/info", "Краткое описание бота.");
+        commandsMap.put("/help", "Список команд.");
+    }
+    
     public static void handleMessage(TelegramBot bot, Message message) {
         String chatId = message.chat().id().toString();
         String text = message.text();
@@ -22,6 +34,14 @@ public class MessageHandlers {
         if ("/info".equals(text)) {
             bot.execute(new SendMessage(chatId, "Этот бот создан для удобной публикации и отслеживания объявлений в канале Барахолка УрФУ."));
             bot.
+        }
+
+        if ("/help".equals(text)) {
+            StringBuilder helpMessage = new StringBuilder("Доступные команды:\n");
+            commandsMap.forEach((command, description) -> 
+                helpMessage.append(command).append(": ").append(description).append("\n")
+            );
+            bot.execute(new SendMessage(chatId, helpMessage.toString()));
         }
     }
 }
