cd17edf15a7c975ba1060d4215d186524287756b
DimaSculp <dimakuhtey1@gmail.com>
2024-10-30 09:41:13 +0500

    добавил классы для колбэков

---
+++ src/main/java/bot/Callbacks/MenuCallback.java
@@ -0,0 +1,16 @@
+package bot.Callbacks;
+
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+
+public class MenuCallback implements BotCallbacks {
+    @Override
+    public String getContent() {
+        return null;
+    }
+
+    @Override
+    public InlineKeyboardMarkup getKeyboard() {
+        return null;
+    }
+
+}
